6ae28fb4821cbb35ad30ea4a89e6402b6a993951
jianpark83 <jian.park83@gmail.com>
2025-04-03 17:54:59 +0900

    Add files via upload

---
+++ 8일차/functionExam04.js
@@ -0,0 +1,27 @@
+
+//함수 표현식 - 화살표함수(람다함수)
+const ramda = (num) => {
+    return num*num;
+};
+result = ramda(9);
+console.log(`곱셈 결과 : ${result}`);
+console.log("------------------------");
+
+
+//매개변수가 1개인 경우는 ()로 생략 가능능
+const ramda2 = num => {
+    return num*num;
+};
+result = ramda2(2);
+console.log(`곱셈 결과 : ${result}`);
+console.log("------------------------");
+
+
+//중괄호 안 문장이 1개인 경우는 중괄호 생략 가능
+//단, 중괄호 안 return 문장이 있으면 삭제
+const ramda3 = num => num*num;
+result = ramda3(8);
+console.log(`곱셈 결과 : ${result}`);
+console.log("------------------------");
+
+
